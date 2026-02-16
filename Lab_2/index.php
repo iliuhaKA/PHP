5fa1dab04eaf8cd87aa30ac08ea7350a3204e490
Илья <iliuha.k.a@gmail.com>
2026-02-16 23:51:08 +0200

    Сделаны задания из лабы

---
+++ Lab_2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Привет, мир! <br/>";
+
+echo "Hello, World with echo! <br/>";
+print "Hello, World with print! <br/>";
+
+$days = 288;
+$message = "Все возвращаются на работу! <br/> ";
+
+echo $days . $message;
+echo "Через $days дней <br/> $message";
+
+?>
